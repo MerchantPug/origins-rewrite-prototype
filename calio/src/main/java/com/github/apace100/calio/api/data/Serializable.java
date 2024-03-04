7cf201eb7de54a85ae975da6399f0a515b403c9a
eggohito <66972816+eggohito@users.noreply.github.com>
2024-03-04 21:30:59 +0800

    Calio: Add `Serializable` interface ...used for dispatching a codec when (de)serializing objects from a registry

---
+++ calio/src/main/java/com/github/apace100/calio/api/data/Serializable.java
@@ -0,0 +1,70 @@
+package com.github.apace100.calio.api.data;
+
+import com.github.apace100.calio.api.IdentifierAlias;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.Lifecycle;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryInfo;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.dynamic.Codecs;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ *  <p>An interface used for dispatching a {@link Codec codec} to (de)serialize the instance of the implementing class. This is mostly used
+ *  for serializable objects in registries (e.g: objects from a {@link net.fabricmc.fabric.api.event.registry.DynamicRegistries dynamic registry}.)
+ *  </p>
+ *
+ *  <p>TODO: Maybe document this interface better? -eggohito</p>
+ */
+public interface Serializable<T> {
+
+    Serializer<? extends T> getSerializer();
+
+    interface Serializer<T> {
+        Codec<? extends T> getCodec();
+    }
+
+    static <T extends Serializable.Serializer<?>> Codec<T> registryCodec(@NotNull Registry<T> registry, @NotNull IdentifierAlias aliases) {
+        return registryCodec(registry, aliases, new IdentifierAlias.Resolver[] {});
+    }
+
+    static <T extends Serializable.Serializer<?>> Codec<T> registryCodec(@NotNull Registry<T> registry, @NotNull IdentifierAlias aliases, IdentifierAlias.Resolver... resolvers) {
+
+        RegistryKey<? extends Registry<T>> registryKey = registry.getKey();
+        IdentifierAlias.Resolver[] finalResolvers = resolvers.length == 0
+            ? IdentifierAlias.Resolver.values()
+            : resolvers;
+
+        return Codecs.withLifecycle(
+            Identifier.CODEC.comapFlatMap(
+                id -> registry.getEntry(aliases.resolveAliasUntil(id, registry::containsId, finalResolvers))
+                    .or(() -> registry.getEntry(IdentifierAlias.GLOBAL.resolveAliasUntil(id, registry::containsId, finalResolvers)))
+                    .map(DataResult::success)
+                    .orElseGet(() -> DataResult.error(() -> "Type \"" + id + "\" is not registered in registry \"" + registryKey.getValue() + "\"")),
+                entry ->
+                    entry.registryKey().getValue()
+            ),
+            entry -> registry.getEntryInfo(entry.registryKey())
+                .map(RegistryEntryInfo::lifecycle)
+                .orElse(Lifecycle.experimental())
+        ).flatComapMap(
+            RegistryEntry.Reference::value,
+            value -> registry.getEntry(value) instanceof RegistryEntry.Reference<T> reference
+                ? DataResult.success(reference)
+                : DataResult.error(() -> "Element \"" + value + "\" is not registered in registry \"" + registryKey.getValue() + "\"")
+        );
+
+    }
+
+    record Factory<T>(Identifier id, Serializer<? extends T> serializer) {
+
+    }
+
+    interface Supplier<T> {
+        Factory<? extends T> getFactory();
+    }
+
+}
