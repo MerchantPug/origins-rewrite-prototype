db062be9e5a2b5fdf3d93ed30509571a5031e843
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-03-06 00:04:45 +1100

    Calio: Add identifier method.

--- calio/src/main/java/com/github/apace100/calio/Calio.java
+++ calio/src/main/java/com/github/apace100/calio/Calio.java
@@ -2,6 +2,7 @@ package com.github.apace100.calio;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,4 +14,9 @@ public class Calio implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Calio loaded!");
 	}
+
+	public static Identifier identifier(String path) {
+		return new Identifier(Calio.MODID, path);
+	}
+
 }
