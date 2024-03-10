039e60e2eeb8fd10594505e87da61811be43b829
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-03-10 20:51:05 +1100

    Remove unnecessary connection event.

--- apoli/src/main/java/com/github/apace100/apoli/client/ApoliClient.java
+++ apoli/src/main/java/com/github/apace100/apoli/client/ApoliClient.java
@@ -1,19 +1,13 @@
 package com.github.apace100.apoli.client;
 
 import com.github.apace100.apoli.network.ApoliPackets;
-import com.github.apace100.apoli.registry.ApoliRegistryKeys;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
 public class ApoliClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
         ApoliPackets.registerS2C();
-        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
 
-            client.world.getRegistryManager().get(ApoliRegistryKeys.POWER).forEach(power -> power.init(client.world.getRegistryManager()));
-
-        });
     }
 }
