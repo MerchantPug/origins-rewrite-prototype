c2ea4bccc872a3c4378d0512b56fc4c938e78646
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-03-11 12:46:40 +1100

    Add /apoli commands to /origins.

--- origins/src/main/java/com/github/apace100/origins/Origins.java
+++ origins/src/main/java/com/github/apace100/origins/Origins.java
@@ -1,7 +1,14 @@
 package com.github.apace100.origins;
 
+import com.github.apace100.apoli.command.PowerCommand;
+import com.github.apace100.apoli.command.ResourceCommand;
 import com.github.apace100.calio.api.GlobalIdentifierAlias;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,11 +20,27 @@ public class Origins implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		GlobalIdentifierAlias.INSTANCE.addNamespaceAlias("origins", "apoli");
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			LiteralCommandNode<ServerCommandSource> originsNode = createOriginsNode(dispatcher);
+			PowerCommand.register(registryAccess, originsNode);
+			ResourceCommand.register(registryAccess, originsNode);
+		});
+
 		LOGGER.info("Origins loaded!");
 	}
 
 	public static Identifier identifier(String path) {
 		return new Identifier(Origins.MODID, path);
+	}
+
+	private static LiteralCommandNode<ServerCommandSource> createOriginsNode(CommandDispatcher<ServerCommandSource> dispatcher) {
+		LiteralCommandNode<ServerCommandSource> originsNode = CommandManager
+				.literal("origins")
+				.requires(c -> c.hasPermissionLevel(2))
+				.build();
+		dispatcher.getRoot().addChild(originsNode);
+		return originsNode;
 	}
 
 }
